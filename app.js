395d662671d9e9a4c13f1201a692313b3c1c7cf0
Arnauld Zeh <name@gmail.com>
2023-02-04 18:21:22 +0100

    modif endpoint

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/api/Beta_luzabu", routes);
+app.use("/api", routes);
 
 module.exports = app;
