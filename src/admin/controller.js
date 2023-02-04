674c533aed21270ef1cd0ccc4d40c55d90404cfd
Intello237 <84332081+Intello237@users.noreply.github.com>
2023-02-04 20:32:49 +0100

    Update controller.js

--- src/admin/controller.js
+++ src/admin/controller.js
@@ -1,27 +1,24 @@
-const { Identifiant, bloquer } = require("./models");
-const {
-  validatecardId,
-  validatebloquecardId,
-} = require("../middleware/dataValidation");
+const { Carte, Bloquer } = require("../admin/models");
+const {validatecardId, validatebloquecardId } = require("../middleware/dataValidation");
 
 //Create neww patient
 const createNewcardId = async (req, res) => {
   await validatecardId(req);
   try {
-    const { cardId } = req.body;
+    const { idCarte } = req.body;
 
     //checking if CardId belongs to the system
     //checking if patient already exists
-    const existingcardId = await Identifiant.findOne({ cardId });
+    const existingcardId = await Carte.findOne({ idCarte });
 
     if (existingcardId) {
-      return res.status(404).json({ error: "cardId already exist" });
+      return res.status(404).json({ error: "id Carte already exist" });
     }
-    const newcardId = new Identifiant({
-      cardId,
+    const newcardId = new Carte({
+      idCarte,
     });
     const createdcardId = await newcardId.save();
-    return res.status(200).json({ message: "cardId added successfully" });
+    return res.status(200).json({ message: "id Carte added successfully" });
   } catch (error) {
     throw error;
   }
@@ -31,20 +28,20 @@ const createNewcardId = async (req, res) => {
 const bloquecardId = async (req, res) => {
   await validatebloquecardId(req);
   try {
-    const { cardId } = req.body;
+    const { idCarte } = req.body;
 
-    const existingcardId = await Identifiant.findOne({ cardId });
+    const existingcardId = await Carte.findOne({ idCarte });
 
     //checking if CardId belongs to the blocked collection
     //checking if patient is already blocked
-    const isBlockedcardId = await bloquer.findOne({ cardId });
+    const isBlockedcardId = await Bloquer.findOne({ idCarte });
     if (!existingcardId) {
       return res.status(404).json({ error: "Id not found" });
     } else if (isBlockedcardId) {
       return res.status(200).json({ message: "Id already blocked" });
     } else {
       const newcardId = new bloquer({
-        cardId,
+        idCarte,
       });
       const blockedcardId = await newcardId.save();
       return res.status(200).json({ message: "User blocked successfully" });
