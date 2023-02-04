c1bf5b7ec943eaa61d2357154ef94b7c74c3a49e
Intello237 <84332081+Intello237@users.noreply.github.com>
2023-02-04 20:10:08 +0100

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const patientRoutes = require("../patient");
+const medecinRoutes = require("../medecin");
 const adminRoutes = require("../admin");
 
+//Les différents chemins des utilisateurs
 router.use("/patient", patientRoutes);
+router.use("/medecin", medecinRoutes);
 router.use("/admin", adminRoutes);
 
 module.exports = router;
