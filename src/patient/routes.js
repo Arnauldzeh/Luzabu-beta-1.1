9165b4256a6f540864acfcdaf08dcbe47c9fa06b
Intello237 <84332081+Intello237@users.noreply.github.com>
2023-02-05 23:16:17 +0100

    Update routes.js

--- src/patient/routes.js
+++ src/patient/routes.js
@@ -1,11 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {
-  createNewPatient,
-  authenticatePatient,
-  getProfile,
-  editProfile,
-} = require("./controller");
+const { createNewPatient, authenticatePatient, getProfile, editProfile } = require("./controller");
 // const { validateProfile } = require("../middleware/dataValidation");
 
 const { verifyToken } = require("../middleware/auth");
