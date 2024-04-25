193fb6c77f0887b99c923fe5be1f2f21f4f35b74
ZEH ZE Arnauld Daniel <arnauldzeh740@gmail.com>
2024-04-25 14:23:54 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const app = require("./app");
 const { PORT } = process.env;
-const { generateStats } = require("./statistics/controller");
-const cron = require("node-cron");
+
 //Lancer l'application
 const startApp = () => {
   app.listen(PORT, () => {
     console.log(`Backend running on port ${PORT}`);
-    // Exécutez la fonction de génération de statistiques toutes les 1 minutes
-    cron.schedule("*/1 * * * *", generateStats);
   });
 };
 
