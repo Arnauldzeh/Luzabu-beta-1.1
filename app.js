1fad6c520d4ba614e9549cc3a8590d7fd65e2090
Arnauldzeh <arnauldzeh740@gmail.com>
2024-05-03 23:00:39 +0100

    Make it better

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 //import database configuration
 require("./src/config/db");
+
 const express = require("express");
 const bodyParser = express.json;
 const cors = require("cors");
 const patientRoutes = require("./src/Routes/patient");
 const doctorRoutes = require("./src/Routes/medecin");
 const laboRoutes = require("./src/Routes/Laborantin");
-// Importez d'autres fichiers de routes au besoin
-// const routes = require("./src/Routes");
 
 //create server app
 const app = express();
@@ -15,7 +14,6 @@ const app = express();
 //middlewares
 app.use(cors());
 app.use(bodyParser());
-// app.use("/api", routes);
 
 app.use("/api/patients", patientRoutes);
 app.use("/api/doctors", doctorRoutes);
